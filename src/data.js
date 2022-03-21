79d783248a6af47a8bc746bf6211d1f79ecf11bd
Harrison Scott <72175825+WhyNotScot@users.noreply.github.com>
2022-03-21 18:24:40 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -18,16 +18,16 @@ export const products = [
   {
     id: 4,
     img: "https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/122497843_103610961552169_1396438017259261099_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=e3f864&_nc_ohc=j7QfWnFv1jwAX9XlE3E&_nc_ht=scontent-hou1-1.xx&oh=00_AT89h2e14h2qG7ECxIdOixyNrCEGi3VYQNT-6th9pCPjvA&oe=625CC115",
-    link: "http://lama.dev",
+    link: "http://WhyNotScot.github.io/deployment",
   },
   {
     id: 5,
     img: "https://images.unsplash.com/photo-1553451166-232112bda6f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
-    link: "http://lama.dev",
+    link: "http://WhyNotScot.github.io/deployment",
   },
   {
     id: 6,
     img: "https://www.markspaneth.com/assets/images/blog/_list_image/02_02_18_508408464_AAB_560x292.jpg",
-    link: "http://lama.dev",
+    link: "http://WhyNotScot.github.io/deployment",
   },
 ];
